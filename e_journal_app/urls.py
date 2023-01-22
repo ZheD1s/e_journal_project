107508bba1f6ce048fc0687b15bab58ab8fedb19
Zhetpissov Dias <zhed1s@mail.ru>
2023-01-22 20:51:24 +0600

    Get user type

--- e_journal_app/urls.py
+++ e_journal_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
-from django.views.generic.base import TemplateView
+from .views import *
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', homeView, name='home'),
 ]
--- e_journal_app/views.py
+++ e_journal_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def homeView(request):
+    thisUser = request.user
+    try:
+        thisStudent = Student.objects.get(user_id=thisUser) # Get data from table Student
+        thisUserType = 'Student'
+    except:
+        thisUserType = 'Teacher'
+    return render(request,
+                  template_name='home.html',
+                  context={
+                      'type': thisUserType
+                  }) # context - datas to show on template
