2213e5ef1014a53f4d914ec4e53f89754acee1c0
Zhetpissov Dias <zhed1s@mail.ru>
2023-02-09 19:24:15 +0600

    HomeWork 26.12.2022

--- e_journal_app/admin.py
+++ e_journal_app/admin.py
@@ -2,12 +2,9 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Group)
 admin.site.register(Grade)
 admin.site.register(Exams)
-admin.site.register(Teacher)
 admin.site.register(Attendance)
-admin.site.register(Subject)
 
 
 
@@ -20,9 +17,31 @@ class StudentAdmin(admin.ModelAdmin):
         ordering = ('surname', 'name')
         # sorting by fields
 
-
     def average_grade(self, obj):
         from django.db.models import Avg
         from django.utils.html import format_html
         result = Grade.objects.filter(student=obj).aggregate(Avg('points'))
         return format_html('<b>{}</b>', result['points__avg'])
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name','admission_year')
+    list_filter = ('admission_year',)
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    search_fields = ('surname__startswith',)
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price_kzt')
+    def price_kzt(self, obj):
+        result = Subject.objects.filter(title=obj).first()
+        if result is not None:
+            result = result.price * 22000
+        return str(result) + ' KZT'
+# Только в Админке. В модели не лезть
+# Добавить колонку в Subject price но в kzt
+# Счиать что 1 кредит в Price равен 22000 KZT
