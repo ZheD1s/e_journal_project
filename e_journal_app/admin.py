ee468b92377bf88b560685fdcd6ac4b3e764d98a
Zhetpissov Dias <zhed1s@mail.ru>
2023-02-10 01:42:12 +0600

    add hyperlink to group in student, add filter horizontal

--- e_journal_app/admin.py
+++ e_journal_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import *
+from django import forms
+from django.urls import reverse
+from django.utils.html import urlencode, format_html
 
 # Register your models here.
 admin.site.register(Grade)
@@ -7,10 +10,20 @@ admin.site.register(Exams)
 admin.site.register(Attendance)
 
 
+class StudentAdminForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+    def clean_name(self):
+        if self.cleaned_data['name'] == 'Banana':
+            raise forms.ValidationError('Banana is not a name!')
+        return self.cleaned_data['name']
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'surname', 'group', 'average_grade')
+    form = StudentAdminForm
+    list_display = ('name', 'surname', 'group_link', 'average_grade')
     list_filter = ('group',)
     search_fields = ('surname__startswith',)
     class Meta:
@@ -19,10 +32,14 @@ class StudentAdmin(admin.ModelAdmin):
 
     def average_grade(self, obj):
         from django.db.models import Avg
-        from django.utils.html import format_html
         result = Grade.objects.filter(student=obj).aggregate(Avg('points'))
         return format_html('<b>{}</b>', result['points__avg'])
 
+    def group_link(self, obj):
+        url = (
+            reverse("admin:e_journal_app_group_changelist") + str(obj.group.id)
+        )
+        return format_html("<a href=" + url + ">" + obj.group.name + "</a>")
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
@@ -32,6 +49,7 @@ class GroupAdmin(admin.ModelAdmin):
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
     search_fields = ('surname__startswith',)
+    filter_horizontal = ('subject',)
 
 
 @admin.register(Subject)
@@ -42,6 +60,4 @@ class SubjectAdmin(admin.ModelAdmin):
         if result is not None:
             result = result.price * 22000
         return str(result) + ' KZT'
-# Только в Админке. В модели не лезть
-# Добавить колонку в Subject price но в kzt
-# Счиать что 1 кредит в Price равен 22000 KZT
+
