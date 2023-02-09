47624e0d6aca794edfcd146b8dc67f4e2101b38b
Zhetpissov Dias <zhed1s@mail.ru>
2023-02-09 18:54:26 +0600

    Sorting and searching

--- e_journal_app/admin.py
+++ e_journal_app/admin.py
@@ -14,12 +14,15 @@ admin.site.register(Subject)
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('name', 'surname', 'group', 'average_grade')
-
+    list_filter = ('group',)
+    search_fields = ('surname__startswith',)
     class Meta:
         ordering = ('surname', 'name')
         # sorting by fields
 
+
     def average_grade(self, obj):
         from django.db.models import Avg
+        from django.utils.html import format_html
         result = Grade.objects.filter(student=obj).aggregate(Avg('points'))
-        return result['points__avg']
+        return format_html('<b>{}</b>', result['points__avg'])
